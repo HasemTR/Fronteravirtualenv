95d1c7e1f46c7ec29bc4c2c35f6e495c40f0a4a4
Christian Martínez <jchristianmartinezg@gmail.com>
2022-08-14 03:15:56 -0500

    added php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Somos una organización emergente, apasionados por la innovación en el ámbito de los entornos virtuales.">
+    <meta name="robots" content="index, follow">
+    <title>Frontera Virtualenv</title>
+    <link rel="icon" href="./assets/img/favicon.png">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+        integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
+    <!--Font awesome icons-->
+    <script src="https://kit.fontawesome.com/25247541c0.js" crossorigin="anonymous"></script>
+    <script src="./script/js.js" type="text/javascript" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./script/script.js"></script>
+</head>
+<body>
+    <H1>login</H1>
+    <a href="./login.php">login</a>
+    <a href="./register.php">sign up</a>
+</body>
+</html>
